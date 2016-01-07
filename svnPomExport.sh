d830bd289c7e94b0f88f44414c27bab8a8e77874
Leonard Daume <ldaume@users.noreply.github.com>
2016-01-07 14:37:59 +0100

    Update svnPomExport.sh

--- svnPomExport.sh
+++ svnPomExport.sh
@@ -1,10 +1,11 @@
-# reads a file with svn dirs to pom files like foo/bar/trunk/pom.xml and exports the dir and file structure.
-# usage: svnPomExport.sh <fileWithPaths> <svn_base_uri>
-# example: svnPomExport.sh latestPoms.txt https://svn.example.com/repos/important/
+# scans a svn for pom files like foo/bar/trunk/pom.xml and exports the dir and file structure.
+# usage: svnPomExport.sh <svn_base_uri>
+# example: svnPomExport.sh https://svn.example.com/repos/important/
 
+svn list -R $1 | grep --line-buffered pom.xml > allpoms
 while read p; do
 	DIR=$(dirname "${p}")
 	mkdir -p $DIR
-	svn export --force $2$p
+	svn export --force $1/$p
 	mv pom.xml $DIR
-done <$1
+done <allpoms
